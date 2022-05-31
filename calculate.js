c0da1e06bb2057aec5b8f9170b137752072ed25f
EC2 Default User <ec2-user@ip-172-31-13-199.us-west-1.compute.internal>
2022-05-31 05:23:09 +0000

    calculate2

--- calculate.js
+++ calculate.js
@@ -31,7 +31,7 @@
   let displaynow = $('#displaybox').val();
   let lasttext = displaynow.slice(-1);
   let lasttext2 = displaynow.slice(-2);
-  if (displaynow != "" && displaynow != "0" && lasttext2 != "+0" && lasttext2 != "-0" && lasttext2 != "*0" && lasttext2 != "/0"&lasttext != "+" & lasttext != "-" & lasttext != "*" & lasttext != "/") {
+  if (displaynow != "" && displaynow != "0" && lasttext2 != "+0" && lasttext2 != "-0" && lasttext2 != "*0" && lasttext2 != "/0" & lasttext != "+" & lasttext != "-" & lasttext != "*" & lasttext != "/") {
    $('#displaybox').val(displaynow + addnumber);
   }
  }
@@ -42,42 +42,41 @@
   let lasttext = displaynow.slice(-1);
   let dotlocation = displaynow.lastIndexOf(".");
   if (dotlocation == -1) {
-   if (lasttext != "+" & lasttext != "-" & lasttext != "*" & lasttext != "/" & lasttext != "." & lasttext != "")
-   {
+   if (lasttext != "+" & lasttext != "-" & lasttext != "*" & lasttext != "/" & lasttext != "." & lasttext != "") {
     $('#displaybox').val(displaynow + addnumber);
    }
   }
   else {
    let dotpossibly = displaynow.indexOf("+", dotlocation) != "-1" || displaynow.indexOf("-", dotlocation) != "-1" || displaynow.indexOf("*", dotlocation) != "-1" || displaynow.indexOf("/", dotlocation) != "-1"
    if (dotpossibly == true) {
-    if (lasttext != "+" & lasttext != "-" & lasttext != "*" & lasttext != "/" & lasttext != "." & lasttext != "") 
-    {
+    if (lasttext != "+" & lasttext != "-" & lasttext != "*" & lasttext != "/" & lasttext != "." & lasttext != "") {
      $('#displaybox').val(displaynow + addnumber);
-    }}
-    else {
-     $('#displaybox').val(displaynow);
     }
    }
-   }
-
-
-  function operaterInput() {
-   let addoperater = $(this).val();
-   let displaynow = $('#displaybox').val();
-   let lasttext = displaynow.slice(-1);
-   if (lasttext != "+" & lasttext != "-" & lasttext != "*" & lasttext != "/" & lasttext != ".") {
-    $('#displaybox').val(displaynow + addoperater);
+   else {
+    $('#displaybox').val(displaynow);
    }
   }
+ }
 
-  function reset() {
-   $('#displaybox').val("");
-  }
 
-  function result() {
-   let displaynow = $('#displaybox').val();
-   let displayResult = eval(displaynow);
-   let displayend = Math.round(displayResult*1000000)/1000000;
-   $('#displaybox').val(displayend);
+ function operaterInput() {
+  let addoperater = $(this).val();
+  let displaynow = $('#displaybox').val();
+  let lasttext = displaynow.slice(-1);
+  if (lasttext != "+" & lasttext != "-" & lasttext != "*" & lasttext != "/" & lasttext != ".") {
+   $('#displaybox').val(displaynow + addoperater);
   }
+ }
+
+ function reset() {
+  $('#displaybox').val("");
+ }
+
+ function result() {
+  let displaynow = $('#displaybox').val();
+  let displayResult = eval(displaynow);
+  let displayend = Math.round(displayResult * 1000000) / 1000000;
+  $('#displaybox').val(displayend);
+ }
  
